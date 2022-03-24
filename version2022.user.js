8f0d18d91948ee09ef22c8d2ae871a02612e38fc
ninjakurt93 <martin@willems-kristiansen.dk>
2022-03-24 12:48:36 +0100

    make eslinter happy in tampermonkey

--- version2022.user.js
+++ version2022.user.js
@@ -7,12 +7,13 @@
 // @include		https://v2.dk/*
 // @include		http://*.v2.dk/*
 // @version     1
+// @description V2 fix
 // ==/UserScript==
 
 window.addEventListener('load', function() {
-	badClasses=["block-tm-kevel","jobs-horizontal-block","section-header--title","continue-reading-block"]
-	for (var badClassNumber = badClasses.length - 1; badClassNumber >= 0; badClassNumber--) {
-		var classToRemove = document.getElementsByClassName(badClasses[badClassNumber])
+	window.badClasses=["block-tm-kevel","jobs-horizontal-block","section-header--title","continue-reading-block"]
+	for (var badClassNumber = window.badClasses.length - 1; badClassNumber >= 0; badClassNumber--) {
+		var classToRemove = document.getElementsByClassName(window.badClasses[badClassNumber])
 		for (var individualBlock = classToRemove.length - 1; individualBlock >= 0; individualBlock--) {
 			classToRemove[individualBlock].remove()
 		}
